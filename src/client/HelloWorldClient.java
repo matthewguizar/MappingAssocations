38a7762e67ef15d27fcf565a9e884ba24a61f177
Matthew Guizar <matthewrguizar@gmail.com>
2022-10-31 19:02:14 -0700

    adding two guides and two students(students under same guide)

--- src/client/HelloWorldClient.java
+++ src/client/HelloWorldClient.java
@@ -15,7 +15,17 @@ public class HelloWorldClient {
         try {
             transaction.begin();
 
-           
+           Guide g1 = new Guide("2000MO10789", "Mike Lawson", 1000);
+           Guide g2 = new Guide("2000IM10901", "Ian Lamb", 2000);
+
+           Student s1 = new Student("2014JT50123", "John Smith", g1);
+           Student s2 = new Student("2014AL50456", "Amy Gill", g1);
+
+           g1.getStudents().add(s1);
+           g1.getStudents().add(s2);
+        
+           session.persist(g1);
+           session.persist(g2);
 
             transaction.commit();
         } catch (Exception e) {
